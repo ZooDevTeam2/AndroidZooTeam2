ba2f82067327487ac214f2def02c7da026ea6a74
Kevin Vaysse <vayssek@gmail.com>
2016-06-08 15:18:13 +0200

    UPDATE DetailStockActivity.java and associated layout
    Visibility of three attributes and the button suppression

--- app/src/main/java/com/lunaret_seb/hb/lunaret_seb_zoo/stock/DetailStockActivity.java
+++ app/src/main/java/com/lunaret_seb/hb/lunaret_seb_zoo/stock/DetailStockActivity.java
@@ -3,14 +3,38 @@ package com.lunaret_seb.hb.lunaret_seb_zoo.stock;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lunaret_seb.hb.lunaret_seb_zoo.R;
 
-public class DetailStockActivity extends Activity {
+public class DetailStockActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_stock);
+
+        Bundle extra = getIntent().getExtras();
+        Stock stock = (Stock) extra.get("stock");
+
+        final TextView name = (TextView)findViewById(R.id.name);
+        name.setText("Nom : "+stock.getName());
+
+        final TextView quantities = (TextView)findViewById(R.id.quantities);
+        quantities.setText("Quantité : "+stock.getQuantities());
+
+        final TextView textMax = (TextView)findViewById(R.id.max);
+        textMax.setText("Limite de stockage : "+stock.getMaximum());
+
+        Button bouton;
+        bouton= (Button) findViewById(R.id.btm);
+    }
+
+    public void onClickBtnSuppr(View v)
+    {
+        Toast.makeText(this, "Suppression de la fourniture", Toast.LENGTH_LONG).show();
     }
 }
