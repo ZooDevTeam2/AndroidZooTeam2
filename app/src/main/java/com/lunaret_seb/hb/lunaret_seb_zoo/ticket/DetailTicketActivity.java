cd5135b4b4a528acf2dae1d72e44de743081907d
Fadul <fadul.fatouma@gmail.com>
2016-06-09 14:46:11 +0200

    COMPLETE Details for tickets

--- app/src/main/java/com/lunaret_seb/hb/lunaret_seb_zoo/ticket/DetailTicketActivity.java
+++ app/src/main/java/com/lunaret_seb/hb/lunaret_seb_zoo/ticket/DetailTicketActivity.java
@@ -3,6 +3,10 @@ package com.lunaret_seb.hb.lunaret_seb_zoo.ticket;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.lunaret_seb.hb.lunaret_seb_zoo.R;
 
@@ -12,5 +16,19 @@ public class DetailTicketActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail_ticket);
+
+        Bundle extra = getIntent().getExtras();
+        Ticket ticket = (Ticket) extra.get("Ticket");
+
+        final TextView name = (TextView)findViewById(R.id.name);
+        name.setText(ticket.getName());
+
+        Button bouton;
+        bouton= (Button) findViewById(R.id.btm);
     }
+
+    public void onClickBtnSuppr(View v) {
+        Toast.makeText(this, "Suppression du ticket", Toast.LENGTH_LONG).show();
+    }
+
 }
